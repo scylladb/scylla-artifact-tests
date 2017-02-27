e3f4b71fdda95dc105fdf97bbb0a273eedee319b
Amos Kong <amos@scylladb.com>
2017-02-27 16:22:45 +0800

    debian 8: install java-1.8 that's depended by scylla 1.7 and later
    In scylla 1.7, scylla-jmx and scylla-tools depend on java-1.8,
    
    https://github.com/scylladb/scylla/issues/2091
    
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- scylla-artifacts.py
+++ scylla-artifacts.py
@@ -360,6 +360,14 @@ class ScyllaInstallDebian8(ScyllaInstallDebian):
     def setup_ci(self):
         process.run('sudo curl %s -o %s' % (self.sw_repo_src, self.sw_repo_dst),
                     shell=True)
+        process.run('sudo apt-get update')
+        result = process.run('sudo apt-cache show scylla')
+        ver = re.findall("Version: (.*)", result.stdout)[0]
+        if parse_version(ver) >= parse_version('1.7~rc0'):
+            process.run("echo 'deb http://http.debian.net/debian jessie-backports main' > /etc/apt/sources.list.d/jessie-backports.list", shell=True)
+            process.run('sudo apt-get update')
+            process.run('sudo apt-get install -y -t jessie-backports openjdk-8-jre-headless', shell=True)
+            process.run('sudo update-java-alternatives -s java-1.8.0-openjdk-amd64', shell=True)
         self.sw_manager.upgrade()
         return ['scylla']
 
