c10f4d7de68f35f9c340d847d3e266a6b4a970c6
Amos Kong <amos@scylladb.com>
2017-04-13 13:05:39 +0800

    full scylla setup
    Currently we only setup scylla with scylla_io_setup script.
    It's a functional test, so we don't reboot vm after setup.
    
    Signed-off-by: Amos Kong <amos@scylladb.com>

--- scylla-artifacts.py
+++ scylla-artifacts.py
@@ -275,7 +275,7 @@ class ScyllaInstallGeneric(object):
                 e_msg = ('Package %s could not be installed '
                          '(see logs for details)' % os.path.basename(pkg))
                 raise InstallPackageError(e_msg)
-        process.run('/usr/lib/scylla/scylla_io_setup', shell=True)
+        process.run('yes "" | sudo /usr/lib/scylla/scylla_setup', shell=True)
 
         self.srv_manager.start_services()
         self.srv_manager.wait_services_up()
