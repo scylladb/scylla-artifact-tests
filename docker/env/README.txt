ccccae5a33e6aa641532082e1f095bf735cee20d
Amos Kong <amos@scylladb.com>
2020-08-24 16:34:32 +0800

    add f30 docker for run avocado
    Huginn had been upgraded from Fedora 30 to Fedora 32.
    All python2 packages had been removed.
    (2019.1) artifact-test jobs of ubuntu/debian failed for lack of Avocado.
    
    This patch switched Dockerfile from CentOS7 to Fedora30.
    The test job already passed.
    
    Signed-off-by: Amos Kong <amos@scylladb.com>

---
+++ docker/env/README.txt
@@ -0,0 +1,8 @@
+
+### Build Docker Image:
+
+```
+cp ../../install-prereqs.sh .
+cp ../../requirements-python.txt .
+docker build -t amoskong/avocado-f30:v0.1 .
+```
--- install-prereqs.sh
+++ install-prereqs.sh
@@ -1,6 +1,16 @@
-yum install -y epel-release libvirt libvirt-devel pkgconfig gcc git python-devel
-yum install -y python-pip
+# Tested by Fedora 30
+
+yum install -y libvirt libvirt-devel pkgconfig gcc git python-devel xz-devel
+yum install -y openssl-devel gdb-gdbserver
+yum install -y python2-pip
 pip install --upgrade pip
+
+# libvirt-python
+yum install -y libvirt-python
+
+pip install -r requirements-python.txt
+pip install avocado-framework==36.4
+
 pip install https://github.com/amoskong/avocado-ec2.git
 
 # avocado-ec2 is used for ami-test
